d1855aa38f8f4dd633c2d470c96fbc22944a1947
DESKTOP-SJ90ONP\Степан <kuzovovstepan1996@yandex.ru>
2018-07-06 00:38:51 +0300

    123

--- store/setting.js
+++ store/setting.js
@@ -1,14 +1,14 @@
 export default {
     state: {
         setting: {
-            phone: '+7 (978) 855-61-84',
+            phone: '+7 (978) 881-41-80',
             email: 'xolms@bmb-design.ru',
             facebook: 'http://www.facebook.com/pages/Vipclass/535538699830079',
             vk: 'http://vk.com/vipclas',
             twitter: 'https://twitter.com/VipClassLLC',
             skype: 'vipclass.vipclass?call',
             homePhone: '+7 (8692) 41-60-00',
-            cellPhone: '+7 (978) 881-41-80',
+            cellPhone: '+7 (978) 855-61-84',
             timeWork: 'Пн-Пт: 8:00-17:00',
             address: 'г. Севастополь, ул. Кесаева 14-Г',
             cellAdress: 'г. Севастополь, пр.Античный 26, (цокольный этаж)'
